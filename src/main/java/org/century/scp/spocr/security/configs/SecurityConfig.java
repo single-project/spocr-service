33a6b60989e21b126446e520ac31e503d4d512da
akurutin <akurutin@gk21.ru>
2020-01-05 15:33:27 +0300

    added redirect from root /api to /actuator/mappings

--- src/main/java/org/century/scp/spocr/security/configs/SecurityConfig.java
+++ src/main/java/org/century/scp/spocr/security/configs/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   public void configure(WebSecurity web) throws Exception {
-    web.ignoring().antMatchers("/h2-console/**", "/actuator/health");
+    web.ignoring().antMatchers("/h2-console/**", "/actuator/health", "/actuator/mappings");
   }
 
   @Override
