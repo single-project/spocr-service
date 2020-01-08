762b57757b3d85eacea0dc1a8806d305a1c3a2bc
akurutin <akurutin@gk21.ru>
2020-01-08 09:58:58 +0300

    cors fixed: return Allow origin in response header

--- src/main/java/org/century/scp/spocr/security/configs/SecurityConfig.java
+++ src/main/java/org/century/scp/spocr/security/configs/SecurityConfig.java
@@ -1,6 +1,5 @@
 package org.century.scp.spocr.security.configs;
 
-import com.google.common.collect.ImmutableList;
 import org.century.scp.spocr.security.services.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -16,9 +15,6 @@ import org.springframework.security.crypto.password.MessageDigestPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.web.context.request.RequestContextListener;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @ServletComponentScan
@@ -54,8 +50,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.httpBasic().disable()
         .csrf().disable()
-        .cors()
-        .and()
         .authorizeRequests()
         .antMatchers("/auth/signin")
         .permitAll()
@@ -79,21 +73,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
   }
 
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    final CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(ImmutableList.of("*"));
-    configuration.setAllowedMethods(ImmutableList.of("HEAD",
-        "GET", "POST", "PUT", "DELETE", "PATCH"));
-    // setAllowCredentials(true) is important, otherwise:
-    // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request credentials mode is 'include'.
-    configuration.setAllowCredentials(true);
-    // setAllowedHeaders is important! Without it, OPTIONS preflight request
-    // will fail with 403 Invalid CORS request
-    configuration
-        .setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
-    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-  }
 }
