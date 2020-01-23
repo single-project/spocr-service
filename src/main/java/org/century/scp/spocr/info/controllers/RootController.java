a48d61d460c0bfc4be572c237486f495d614749f
akurutin <akurutin@gk21.ru>
2020-01-23 12:19:12 +0300

    #10 added simple settings controller - app and user.

--- src/main/java/org/century/scp/spocr/info/controllers/RootController.java
+++ src/main/java/org/century/scp/spocr/info/controllers/RootController.java
@@ -1,5 +1,7 @@
 package org.century.scp.spocr.info.controllers;
 
+import org.century.scp.spocr.info.models.dto.SettingsView;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class RootController {
 
-  @GetMapping("/{id}")
+  @GetMapping()
   public String getAllEndpoints() {
     return "redirect:/actuator/mappings";
+  }
+
+  @GetMapping("/app/settings")
+  public ResponseEntity<SettingsView> getAppSettings() {
+    return ResponseEntity.ok(new SettingsView());
+  }
+
+  @GetMapping("/user/settings")
+  public ResponseEntity<SettingsView> geUserSettings() {
+    return ResponseEntity.ok(new SettingsView());
   }
 }
