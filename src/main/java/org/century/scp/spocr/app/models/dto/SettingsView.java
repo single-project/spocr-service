94223e7aa2f92f897cf9230c9bea4c03f777ff57
akurutin <akurutin@gk21.ru>
2020-02-27 12:40:55 +0300

    added new legal types in configuration

--- src/main/java/org/century/scp/spocr/app/models/dto/SettingsView.java
+++ src/main/java/org/century/scp/spocr/app/models/dto/SettingsView.java
@@ -1,6 +1,7 @@
 package org.century.scp.spocr.app.models.dto;
 
 import java.util.List;
+import java.util.Map;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,4 +12,8 @@ public class SettingsView {
   private int defaultDataType;
 
   private List<TableConfig> availableTables;
+
+  private Map<String, String> daDataConfiguration;
+
+  private List<Map<String, String>> newLegalTypes;
 }
