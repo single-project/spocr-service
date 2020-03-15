d8afec76d50becd6c4cff210565ba591553bed67
akurutin <akurutin@gk21.ru>
2020-03-15 11:57:22 +0300

    added entity types

--- src/main/java/org/century/scp/spocr/extlink/models/EntityType.java
+++ src/main/java/org/century/scp/spocr/extlink/models/EntityType.java
@@ -5,9 +5,20 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum EntityType {
+  CONTACT,
+  CONTACT_ROLE,
+  CONTRACT,
+  SUB_CONTRACT,
+  OWNER,
   SHOP,
   SHOP_TYPE,
-  MANUFACTURER;
+  SALES_CHANNEL,
+  SHOP_DEPART,
+  SHOP_SPECIALIZATION,
+  MANUFACTURER,
+  COUNTERPARTY,
+  EXT_REG_SYSTEM,
+  ;
 
   public static List<String> getTypes() {
     return Stream.of(values()).map(Enum::name).collect(Collectors.toList());
